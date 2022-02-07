8860a6aa77eb3bac14e48a1ef7055ba4d2883932
60manojkumar@gmail.com <Motivated@2019>
2022-02-07 08:51:21 +0530

    for loop working

---
+++ src/loop_working/Demo.java
@@ -0,0 +1,32 @@
+package loop_working;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+
+        /*System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");*/
+
+        // SYNTAX:  for(initilization ;    condition;  increment){
+        ////                                      code to be executed
+        //                                                            }
+
+        for (int i=0; i<5; i++){
+            System.out.println("increment Hello "+i);
+
+        }
+
+        // SYNTAX:  for(initilization ;    condition;  decrement){
+        ////                                      code to be executed
+        //
+
+        for (int i=5; i>0 ; i--){
+            System.out.println("Decrement Hello "+i);
+        }
+
+    }
+}
---
+++ src/loop_working/TableOfTwo.java
@@ -0,0 +1,17 @@
+package loop_working;
+
+public class TableOfTwo {
+
+    public static void main(String[] args) {
+
+        int n=2;
+
+
+        for (int i=1; i<=10; i++){
+
+            int result =n*i;
+            System.out.println("Table of Two : "+result);
+        }
+
+    }
+}
